067a947b84b75f3b393ba250e440007b2653d640
fletchgraham <sfletchgraham@gmail.com>
2019-12-31 12:30:11 -0500

    make a function for getting html from markdown filepath.

--- generate.py
+++ generate.py
@@ -4,31 +4,37 @@ import markdown
 
 def main():
     page = os.path.join('pages', 'digital_sculptures')
+    dst = 'site'
 
-    process_page(page)
+    process_page(page, dst)
 
-def process_page(folder):
+def process_page(src, dst):
+    """Create a webpage in the destination based on markdown in the source"""
+    md_filepath = find_md_file(src)
+    html = get_html(md_filepath)
 
-    md_file = find_md_file(folder)
+    print(html)
 
-    with open(md_file, 'r') as infile:
+def get_html(md_filepath):
+    """Return html from the given markdown filepath"""
+
+    with open(md_filepath, 'r') as infile:
         md = infile.read()
 
     html = markdown.markdown(md)
 
-    print(html)
-
+    return html
 
 def find_md_file(folder):
     """Return path to the markdown file in a folder"""
 
-    md_file = None
+    md_filepath = None
 
     for item in os.listdir(folder):
         if os.path.splitext(item)[1].lower() == '.md':
-            md_file = os.path.join(folder, item)
+            md_filepath = os.path.join(folder, item)
 
-    return md_file
+    return md_filepath
 
 
 if __name__ == '__main__':
