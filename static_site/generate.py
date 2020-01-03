b2b657c628e5e8ba7d33bc7ec7ae73693e8320bb
fletchgraham <sfletchgraham@gmail.com>
2020-01-02 21:52:21 -0500

    consolidate, code wasn't complex enough to warrant so many pesky functions

--- static_site/generate.py
+++ static_site/generate.py
@@ -1,28 +1,45 @@
-import os
+"""Generate a static html site from a bunch of markdown files."""
 
+import os
 import markdown
 
 def main():
+
     print('install worked')
 
-def get_pages(folder):
-    """Return filepaths to page folders"""
-    pages = []
-    for item in os.listdir(folder):
-        item_path = os.path.join(folder, item)
-        if not os.path.isdir(item_path):
-            continue
-        if not find_md_file(item_path):
-            continue
-        pages.append(item_path)
-    return pages
 
-def process_page(src, dst):
-    """Create a webpage in the destination based on markdown in the source"""
-    md_filepath = find_md_file(src)
-    html = get_html(md_filepath)
+def process(src, dst):
+    """If the src is a valid page,
+    process it and spit out a webpage."""
 
-    print(html)
+    # if it's not a directory, get out early
+    if not os.path.isdir(src):
+        return
+
+    # get a list of md files in the dir
+    md_files = [
+        x for x
+        in os.listdir(src)
+        if os.path.splitext(x)[1].lower() == '.md'
+        ]
+
+    # if no markdown files, get out early
+    if not md_files:
+        return
+
+    # turn the md into html
+    html = get_html(os.path.join(src, md_files[0]))
+
+    page_name = os.path.basename(src)
+    out_folder = os.path.join(dst, page_name)
+    index_path = os.path.join(dst, page_name, 'index.html')
+
+    if not os.path.exists(out_folder):
+        os.makedirs(out_folder)
+
+    with open(index_path, 'w') as outfile:
+        outfile.write(html)
+
 
 def get_html(md_filepath):
     """Return html from the given markdown filepath"""
@@ -33,17 +50,6 @@ def get_html(md_filepath):
     html = markdown.markdown(md)
 
     return html
-
-def find_md_file(folder):
-    """Return path to the markdown file in a folder"""
-
-    md_filepath = None
-
-    for item in os.listdir(folder):
-        if os.path.splitext(item)[1].lower() == '.md':
-            md_filepath = os.path.join(folder, item)
-
-    return md_filepath
 
 
 if __name__ == '__main__':
